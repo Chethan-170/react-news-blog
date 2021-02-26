e43a2daa8b73a26d99b0e719a4aad33cc1b07456
=Chethan-170 <chethanpoojary170@gmail.com>
2021-02-27 01:00:03 +0530

    Minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,24 +1,11 @@
-import { Fragment } from "react";
 
 const Home = ()=>{
     return(
-        <Fragment>
-            <div className="main-content">
-                <div className="individualBlog mt-3  mb-3 ml-4 mr-4">
-                    <p className="p-3">Wiki is Hawaiian for “to hurry; fast, quick.” As a noun, wiki means “a website that allows anyone to add, delete, or revise content by using a web browser.” The term wiki is also used as a verb; if you wiki, you are either researching a topic on a wiki or contributing to one.</p>
-                </div>
-            </div>
-            <div className="main-content">
-                <div className="individualBlog mt-3  mb-4 ml-4 mr-4">
-                    <p className="p-3">Wiki is Hawaiian for “to hurry; fast, quick.” As a noun, wiki means “a website that allows anyone to add, delete, or revise content by using a web browser.” The term wiki is also used as a verb; if you wiki, you are either researching a topic on a wiki or contributing to one.</p>
-                </div>
-            </div>
-            <div className="main-content">
-                <div className="individualBlog mt-3  mb-3 ml-4 mr-4">
-                    <p className="p-3">Wiki is Hawaiian for “to hurry; fast, quick.” As a noun, wiki means “a website that allows anyone to add, delete, or revise content by using a web browser.” The term wiki is also used as a verb; if you wiki, you are either researching a topic on a wiki or contributing to one.</p>
-                </div>
-            </div>
-        </Fragment>
+        <div className="mainContent">
+            <p className="p-3 individualBlog1">Wiki is Hawaiian for “to hurry; fast, quick.” As a noun, wiki means “a website that allows anyone to add, delete, or revise content by using a web browser.” The term wiki is also used as a verb; if you wiki, you are either researching a topic on a wiki or contributing to one.</p>
+            <p className="p-3 individualBlog2">Wiki is Hawaiian for “to hurry; fast, quick.” As a noun, wiki means “a website that allows anyone to add, delete, or revise content by using a web browser.” The term wiki is also used as a verb; if you wiki, you are either researching a topic on a wiki or contributing to one.</p>  
+            <p className="p-3 individualBlog3">Wiki is Hawaiian for “to hurry; fast, quick.” As a noun, wiki means “a website that allows anyone to add, delete, or revise content by using a web browser.” The term wiki is also used as a verb; if you wiki, you are either researching a topic on a wiki or contributing to one.</p>          
+        </div>
     );
 }
 export default Home;
