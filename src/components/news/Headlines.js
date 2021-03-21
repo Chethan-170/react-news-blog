1974c502fb059de6c0b0aa64ca622edc480d85d2
=Chethan-170 <chethanpoojary170@gmail.com>
2021-03-21 10:32:46 +0530

    features updated

--- src/components/news/Headlines.js
+++ src/components/news/Headlines.js
@@ -1,16 +1,81 @@
 import { containerStyle } from '../style';
 import { connect } from 'react-redux';
+import { useState, useEffect } from 'react';
+import { Loader } from '../Loader';
+import { NavLink } from 'react-router-dom';
 
 const HeadLines = (props)=>{
-    console.log("headlines",props);
-    return(
-        <div className="" style={containerStyle}>
-            <h2>Headlines here goes..</h2>
+    const [headLines,setHeadLines] = useState([]);
+    useEffect(()=>{
+        setHeadLines(props.headLines);
+        console.log("headlinesstate",headLines);
+    },[props.headLines, headLines]);
+    return(        
+        (headLines.length > 0)
+        ?
+        <div className="news-card-container">
+            <div className="news-cards">
+                <NavLink className="news-card" to="viewNews/21">
+                    <span className="news-card-header" style={{backgroundImage: 'url(http://placeimg.com/400/200/animals)'}}>
+                        <span className="news-card-title">
+                            <h3>This is a title for a card</h3>
+                        </span>
+                    </span>
+                    <span className="news-card-summary">
+                        A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+                    </span>
+                    <span className="news-card-meta">
+                        Published: June 18th, 2015
+                    </span>
+                </NavLink>
+                <NavLink className="news-card" to="viewNews/21">
+                    <span className="news-card-header" style={{backgroundImage: 'url(http://placeimg.com/400/200/animals)'}}>
+                        <span className="news-card-title">
+                            <h3>This is a title for a card</h3>
+                        </span>
+                    </span>
+                    <span className="news-card-summary">
+                        A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+                    </span>
+                    <span className="news-card-meta">
+                        Published: June 18th, 2015
+                    </span>
+                </NavLink>
+                <NavLink className="news-card" to="viewNews/21">
+                    <span className="news-card-header" style={{backgroundImage: 'url(http://placeimg.com/400/200/animals)'}}>
+                        <span className="news-card-title">
+                            <h3>This is a title for a card</h3>
+                        </span>
+                    </span>
+                    <span className="news-card-summary">
+                        A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+                    </span>
+                    <span className="news-card-meta">
+                        Published: June 18th, 2015
+                    </span>
+                </NavLink>
+                <NavLink className="news-card" to="viewNews/21">
+                    <span className="news-card-header" style={{backgroundImage: 'url(http://placeimg.com/400/200/animals)'}}>
+                        <span className="news-card-title">
+                            <h3>This is a title for a card</h3>
+                        </span>
+                    </span>
+                    <span className="news-card-summary">
+                        A summary will also be present. Usually two to three brief sentences about the content on the detail page.
+                    </span>
+                    <span className="news-card-meta">
+                        Published: June 18th, 2015
+                    </span>
+                </NavLink>
         </div>
+    </div>
+        :
+            <Loader/>
+        
     )
 }
 function mapStateToProps(state){
     const { headLines } = state;
-    return { headlines : headLines }
+    return { headLines }
 }
 export default connect(mapStateToProps)(HeadLines);
--- src/components/style.js
+++ src/components/style.js
@@ -2,4 +2,4 @@ export const containerStyle = {
     color : "white",
     backgroundColor : "grey",
     padding : 10
-}
+ }
