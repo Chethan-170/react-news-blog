9ac2be72d31650f52b31c3a955e650541115a8ad
=Chethan-170 <chethanpoojary170@gmail.com>
2021-03-25 22:44:51 +0530

    features updated

--- src/components/news/Viewnews.js
+++ src/components/news/Viewnews.js
@@ -2,8 +2,26 @@ import { connect } from 'react-redux';
 
 const ViewNews = ({ news }) =>{
     console.log(news);
+    const dateArr = new Date(news.publishedAt).toDateString().split(' ');
     return (
-        <h2>{ news.title }</h2>
+        <div className="post-module hover">
+            <div className="thumbnail">
+                <div className="date">
+                <div className="day">{ dateArr[2] }</div>
+                <div className="month">{ dateArr[1] }</div>
+                </div><img alt="Alt Img" src={news.urlToImage}/>
+            </div>
+            <div className="post-content">
+                <div className="category">{ news.author }</div>
+                <h1 className="title">{ news.title }</h1>
+                <h2 className="sub_title">{ news.description }</h2>
+                <p className="description">
+                   { news.content }
+                </p>
+                <div className="post-meta"><span className="timestamp"><i className="fa fa-clock-o"></i>
+                    {Math.floor((Math.random() * 10) + 1)} mins ago</span><span className="comments"><i className="fa fa-comments"></i><a href="hi">{Math.floor((Math.random() * 100) + 1)} comments</a></span></div>
+            </div>
+        </div>
     )
 }
 const mapStateToProps = (state, ownProps)=>{
